fab93cd3a36ce6cfac20ff6a2199b1fb5e56e0ad
Nicholas van de Walle <nicholas@niceguys.co>
2015-11-18 11:19:13 -0800

    Reorganize app.js
    Just because i think this is clearer

--- backend/app.js
+++ backend/app.js
@@ -1,35 +1,32 @@
 #!/usr/bin/env node
 
-var express = require('express');
 var path = require('path');
-var app = express();
-var commandLineArgs = require('command-line-args');
-var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var commandLineArgs = require('command-line-args');
+
 require('./models/url');
+var app = require('express')();
+var urlRoutes = require('./routes/urls');
+
 
 // Parse command line args
 var cli = commandLineArgs([
-  {name: 'config', alias: 'c', type: String, defaultValue: './config'}
+  { name: 'config', alias: 'c', type: String, defaultValue: './config' }
 ]);
 var options = cli.parse(process.argv);
+var port = process.env.PORT || 3000; // TODO: Add command line option as well
 
-// Get the config
 var config = require('./' + options.config);
 
-// Connect to MongoDB
 mongoose.connect(config.db.url);
 
-// Setup express extensions
+// Setup express middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Start the server
-var port = process.env.PORT || 3000;
-var server = app.listen(port);
-
-// Setup routes
-var urlRoutes = require('./routes/urls');
+// Routes
 app.use('/', urlRoutes);
 
-module.exports = app;
+// Initialize Server
+app.listen(port);
